20bed75719ac43d7681303636fdf55829ea86db4
changkwon cho <ckcho231@gmail.com>
2022-07-28 11:36:21 +0900

    Changes

--- test3.py
+++ test3.py
@@ -1,19 +1,7 @@
-class FourCal:
-    def __init__(self):
-        self.fir = None
-        self.sec = None
 
-    def setdata(self, fir, sec):
-        self.fir = fir
-        self.sec = sec
+from ckcho import fourCal
 
-    def add(self):
-        result = self.fir + self.sec
-        return result
-
-
-
-a = FourCal()
+a = fourCal()
 a.setdata(4, 2)
 print(a.add())
-print("hello world")
+
