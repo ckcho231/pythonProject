e7a637c1a796990782c6527749f799c3b5712f46
changkwon cho <ckcho231@gmail.com>
2023-06-17 23:42:36 +0900

    Changes

--- test4.py
+++ test4.py
@@ -4,3 +4,10 @@ while treeHit < 10:
     print("%d 번 찍었습니다" % treeHit)
     if treeHit == 10:
         print("넘어간다~~~~")
+
+var_char='안녕하세요'
+num1=10000
+
+print('This is text %s , This is number %d'%(var_char, num1))
+
+lux = {'health': 490, 'health': 800, 'mana': 334, 'melee': 550, 'armor': 18.72}
